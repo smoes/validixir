37390abdd4f7008fa8493069e256ff363b8c5777
Simon Härer <simon.haerer@active-group.de>
2022-09-30 22:08:37 +0200

    Add failure tests

--- lib/validex/error.ex
+++ lib/validex/error.ex
@@ -3,23 +3,58 @@ defmodule Validex.Error do
 
   @type t :: %Error{}
   @enforce_keys [:candidate, :message]
-  defstruct [:candidate, :message, :label]
+  defstruct [:candidate, :message, :context]
 
+  @doc ~S"""
+  Smart constructor of an error.
+  """
   @spec make(any(), any(), any()) :: t()
-  def make(candidate, message, label),
-    do: %Error{candidate: candidate, message: message, label: label}
+  def make(candidate, message, context),
+    do: %Error{candidate: candidate, message: message, context: context}
 
-  @spec with_label(t(), any()) :: t()
-  def with_label(error, label), do: %Error{error | label: label}
+  @doc ~S"""
+  Overrides the context of an error
 
+  ## Examples
+
+      iex> Validex.Error.with_context(Validex.Error.make(12, :message, Context), NewContext)
+      %Validex.Error{candidate: 12, message: :message, context: NewContext}
+  """
+  @spec with_context(t(), any()) :: t()
+  def with_context(error, context), do: %Error{error | context: context}
+
+  @doc ~S"""
+  Overrides the message of an error
+
+  ## Examples
+
+      iex> Validex.Error.with_message(Validex.Error.make(12, :message, Context), :new_message)
+      %Validex.Error{candidate: 12, message: :new_message, context: Context}
+  """
   @spec with_message(t(), any()) :: t()
   def with_message(error, message), do: %Error{error | message: message}
 
+  @doc ~S"""
+  Augments the message of an error
+
+  ## Examples
+
+      iex> Validex.Error.augment_message(Validex.Error.make(12, :message, Context), :new_message)
+      %Validex.Error{candidate: 12, message: {:new_message, :message}, context: Context}
+  """
   @spec augment_message(t(), any()) :: t()
   def augment_message(error = %Error{message: message}, additional_message),
     do: with_message(error, {additional_message, message})
 
-  @spec augment_label(t(), any()) :: t()
-  def augment_label(error = %Error{label: label}, additional_label),
-    do: with_label(error, {additional_label, label})
+  @doc ~S"""
+  Augments the context of an error
+
+  ## Examples
+
+      iex> Validex.Error.augment_context(Validex.Error.make(12, :message, Context), NewContext)
+      %Validex.Error{candidate: 12, message: :message, context: {NewContext, Context}}
+  """
+  @spec augment_context(t(), any()) :: t()
+  def augment_context(error = %Error{context: context}, additional_context),
+    do: with_context(error, {additional_context, context})
 end
---
+++ test/validatex/failure_test.exs
@@ -0,0 +1,4 @@
+defmodule Validex.FailureTest do
+  use ExUnit.Case
+  doctest Validex.Failure
+end
