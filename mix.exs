854960df7a999a9d385157a0e243edf4665a7767
Simon Härer <simon.haerer@bravobike.de>
2024-04-05 12:16:48 +0200

    Increase version to 1.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Validixir.MixProject do
   def project do
     [
       app: :validixir,
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: "~> 1.13",
       package: package(),
       start_permanent: Mix.env() == :prod,
